c644b09d6b8c52fdf47dd675ac4090faebfa3690
Tidwell <jontidwell@FVFZMY9SJ1WK.tld>
2022-04-07 14:19:09 -0500

    FINAL COMMIT

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ userNum.focus();
 let counter = 0;
 
 function run(){
-    randumNum();
+    randomNum();
     userNumber();
     compareNumbers();
 }
@@ -29,35 +29,35 @@ function compareNumbers() {
     let a = userNumber();
     let b = randomNum();
     let z = document.getElementById("compare");
-    let c = document.getElementById("counter")
+    let c = document.getElementById("counter");
 
-    if (a!= b) {
+    if (a != b) {
         z.innerHTML = "Number are not the same. Computer got "  + b + ", and user got " + a;
-        z.style.color = "purple";
-        z.style.backgroundColor = "blue";
+        z.style.color = "white";
+        z.style.backgroundColor = "red";
         z.style.padding = "20px";
         z.style.textAlign = "center";
         counter++;
-        c.innerHTML = "You've tried " + counter + " times."
-        c.style.color = "pink";
-        c.style.backgroundColor = "blue";
+        c.innerHTML = "You've tried " + counter + " times.";
+        c.style.color = "white";
+        c.style.backgroundColor = "red";
         c.style.padding = "20px";
         c.style.textAlign = "center";
         document.querySelector("body").style.backgroundColor = "red";
     
-    } else if( a == b) {
+    } else if(a == b) {
         z.innerHTML = "The numbers are the same. Computer got " + b +", and user got " + a;
-        z.style.color = "purple";
-        z.style.backgroundColor = "blue";
+        z.style.color = "white";
+        z.style.backgroundColor = "green";
         z.style.padding = "20px";
         z.style.textAlign = "center";
-        c.innerHTML = "You have tried " + counter + " times to get it right!"
-        c.style.color = "pink";
-        c.style.backgroundColor = "blue";
+        c.innerHTML = "You have tried " + counter + " times to get it right!";
+        c.style.color = "white";
+        c.style.backgroundColor = "green";
         c.style.padding = "20px";
         c.style.textAlign = "center";
         counter = 0;
-        document.querySelector("body").style.backgroundColor = "green"
+        document.querySelector("body").style.backgroundColor = "green";
     }
 
     resetInput();
